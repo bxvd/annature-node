a8b669f29602a941ac1bcdc48002e40c6411a6f4
Brodie Daff <brodie@bxvd.dev>
2022-07-23 00:01:17 +1000

    Add Accounts and Documents

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,5 @@
+import { AxiosResponse } from 'axios';
+
 export const snakeCase = (o?: { [key: string]: any }) => {
   const newObj: { [key: string]: any } = {};
 
@@ -10,8 +12,8 @@ export const snakeCase = (o?: { [key: string]: any }) => {
   return newObj;
 };
 
-export const camelCase = (o?: { [key: string]: any }) => {
-  const newObj: { [key: string]: any } = {};
+export const camelCase: <T>(o: any) => T = <T>(o?: { [key: string]: any }) => {
+  const newObj: typeof o = {};
 
   if (o) {
     for (const key of Object.keys(o)) {
@@ -19,5 +21,55 @@ export const camelCase = (o?: { [key: string]: any }) => {
     }
   }
 
-  return newObj;
+  return newObj as T;
+};
+
+export const isoStringifyDates = (o?: { [key: string]: any }) => {
+  let newObj: typeof o = {};
+
+  if (o) {
+    newObj = { ...o };
+
+    for (const key of Object.keys(o)) {
+      if (o[key] instanceof Date) {
+        newObj[key] = o[key].toISOString();
+      }
+    }
+  }
+
+  return newObj ?? {};
+};
+
+export const datifyIsoStrings = (o?: { [key: string]: any }) => {
+  let newObj: typeof o = {};
+
+  if (o) {
+    newObj = { ...o };
+
+    for (const key of Object.keys(o)) {
+      if (['created', 'verified', 'sent', 'voided', 'declined', 'completed', 'viewed', 'failed'].includes(key)) {
+        newObj[key] = new Date(o[key]);
+      }
+    }
+  }
+
+  return newObj ?? {};
+};
+
+export const handleDataResponse: <T>(apiCall: Promise<AxiosResponse>) => Promise<T> = async apiCall => {
+  try {
+    const res = await apiCall;
+
+    return Array.isArray(res.data) ? res.data.map(v => datifyIsoStrings(v)) : (datifyIsoStrings(res.data) as any);
+  } catch (error: any) {
+    throw error?.response?.data?.message ? Error(error?.response?.data?.message) : error;
+  }
+};
+
+export const handleDatalessResponse: (apiCall: Promise<AxiosResponse>) => Promise<void> = async apiCall => {
+  try {
+    await apiCall;
+  } catch (error: any) {
+    throw error?.response?.data?.message ? Error(error?.response?.data?.message) : error;
+  }
 };
