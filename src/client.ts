ef86ac03bf9ef0e5ecb06f680d4d7a919e1cd7cd
Brodie Daff <brodie@bxvd.dev>
2022-07-23 15:02:42 +1000

    Add organisations

--- src/client.ts
+++ src/client.ts
@@ -3,6 +3,10 @@ import accounts from './accounts';
 import documents from './documents';
 import endpoints from './endpoints';
 import envelopes from './envelopes';
+import fields from './fields';
+import groups from './groups';
+import organisations from './organisations';
+import recipients from './recipients';
 
 export type Credentials = {
   id: string;
@@ -17,6 +21,10 @@ export class Annature {
   readonly documents: ReturnType<typeof documents>;
   readonly endpoints: ReturnType<typeof endpoints>;
   readonly envelopes: ReturnType<typeof envelopes>;
+  readonly fields: ReturnType<typeof fields>;
+  readonly groups: ReturnType<typeof groups>;
+  readonly recipients: ReturnType<typeof recipients>;
+  readonly organisations: ReturnType<typeof organisations>;
 
   constructor(credentials: Credentials) {
     this._id = credentials.id;
@@ -36,5 +44,9 @@ export class Annature {
     this.documents = documents(this.client);
     this.endpoints = endpoints(this.client);
     this.envelopes = envelopes(this.client);
+    this.fields = fields(this.client);
+    this.groups = groups(this.client);
+    this.recipients = recipients(this.client);
+    this.organisations = organisations(this.client);
   }
 }
